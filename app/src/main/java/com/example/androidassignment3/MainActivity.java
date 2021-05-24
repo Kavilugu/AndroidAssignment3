a71d48c2cc40983acae4d9e0a42cea5f2160638f
Erik-TW <45933770+Erik-TW@users.noreply.github.com>
2021-05-24 12:27:44 +0200

    added needed classes and started on logic

--- app/src/main/java/com/example/androidassignment3/MainActivity.java
+++ app/src/main/java/com/example/androidassignment3/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.androidassignment3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String NY_TIMES_API_KEY = "UPeQIM5GaD2LWH1hNUogB5wMxFIC0CIG";
+    public static final String OMDB_API_KEY = "84237959";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("TEST", "LOG");
-        Log.e("TEST", "LOG ERIK");
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragTrans = fragmentManager.beginTransaction();
+        fragTrans.add(R.id.container, new SearchFragment());
+        fragTrans.commit();
     }
 }
