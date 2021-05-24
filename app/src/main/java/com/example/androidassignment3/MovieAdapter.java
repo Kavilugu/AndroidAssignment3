a96d60ab2a75bdeab7cef39b8f5cf3d214966024
kavilugu <karlolofnylander@gmail.com>
2021-05-24 15:22:53 +0200

    removed unnecessary imports

--- app/src/main/java/com/example/androidassignment3/MovieAdapter.java
+++ app/src/main/java/com/example/androidassignment3/MovieAdapter.java
@@ -7,12 +7,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.squareup.picasso.Picasso;
-
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
