44f3b0a93548c4da9ed172bd2783daece3dab6d2
Cusatelli <github.cusatelli@gmail.com>
2021-05-26 21:25:05 +0200

    Fixed warnings.

--- app/src/main/java/com/example/androidassignment3/SearchFragment.java
+++ app/src/main/java/com/example/androidassignment3/SearchFragment.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +11,6 @@ import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
